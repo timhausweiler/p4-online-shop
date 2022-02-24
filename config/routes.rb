c3bdd8ac5bfdeca5f9f9a87d5f19205e999a48e4
timhausweiler <haustimp@gmail.com>
2022-02-24 16:04:26 -0500

    delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,4 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
