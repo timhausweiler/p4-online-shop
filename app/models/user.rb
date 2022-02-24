d63d46a7679f102eec874e3cdbcbb411e7ff351a
timhausweiler <haustimp@gmail.com>
2022-02-24 15:21:52 -0500

    installing jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :reviews, dependent: :destroy
-
   has_secure_password
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
