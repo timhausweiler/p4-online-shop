35b325fbef8059821a911d49c83be526de7357b1
timhausweiler <haustimp@gmail.com>
2022-02-24 17:20:40 -0500

    complete login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
+import { loginUser } from '../services/users'
+import { useNavigate } from 'react-router-dom'
 
-export default function Login() {
+export default function Login(props) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
   return (
-    <div>Login</div>
+    <form onSubmit={async (e) => {
+      e.preventDefault()
+      const user = {
+        username,
+        password
+      }
+      const resp = await loginUser(user)
+      props.setCurrentUser(resp)
+
+      navigate('/')
+
+    }}>
+      <input type='text' onChange={(e) => setUsername(e.target.value) } value={username} />
+      <input type='password' onChange={ (e) => setPassword(e.target.value)} value={password} />
+      <button>Login</button>
+    </form>
   )
 }
