c163e1650bab055480905046f77b294e0f1b87f9
timhausweiler <haustimp@gmail.com>
2022-02-24 17:23:27 -0500

    login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
 import { useEffect, useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { verifyUser } from './services/users';
+
+import Login from './components/Login';
 
 function App() {
 
   const [currentUser, setCurrentUser] = useState(null)
 
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await verifyUser()
+      setCurrentUser(user)
+    }
+    getUser()
+  }, [])
+
+
   return (
     <div className="App">
-      
-    
+      <Routes>
+        <Route path='/login' element={<Login setCurrentUser={setCurrentUser} />} />
+      </Routes>
     </div>
   );
 }
